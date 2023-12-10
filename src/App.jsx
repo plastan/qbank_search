4af0b8ebd227e201b5051a046e369da0d116452e
plastan <angelofthousandeyes@gmail.com>
2023-12-10 19:05:56 +0530

    initiated the migration of data from external api to interal load

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {Link, Route, Routes} from 'react-router-dom'
 import {Home} from './Home'
 import {About} from './About'
 
-// import {Cards} from './cards';
 
 export default function App() {
   return( 
--- src/Home.jsx
+++ src/Home.jsx
@@ -13,36 +13,63 @@ const tray = signal([]);
 export  function Home() {
     const [stuffs,setstuffs] = useState([]);  
     let list_of_items = [];
-  
-    function handleQuery(value) {
-      query.value = value;
-  
-      axios.get('http://127.0.0.1:8000/query?s=' + value)
-        .then(function (response) {
-          // handle success
-          // console.log(response.data.data);
-          tray.value = [...response.data.data]
-          setstuffs([...response.data.data])
-          
-          console.log(tray.value.length)
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        })
-        .finally(function () {
-          // always executed
-        });
+    fetch("os.json")
+  .then(response => response.json())
+  .then(json => {
+    // console.log(json);
+    localStorage.setItem("paper",JSON.stringify(json));
+  })
+
+
+
+  const Fdata = JSON.parse(localStorage.paper);
+  // console.log(Fdata);
+  const hmap = new Map(Object.entries(Fdata));
+  // console.log(hmap);
+
+  function search(svalue){
+    
+    for (const [key, value] of hmap) {
+      
+      if (value.q.includes(svalue)) {
+        // console.log(`Key: ${key}, Value: ${value}`); 
+        tray.value=[...tray.value, key];
+      }
     }
+  
+  }
+  
+    // function handleQuery(value) {
+    //    query.value = value;
+  
+    //   axios.get('http://127.0.0.1:8000/query?s=' + value)
+    //     .then(function (response) {
+    //       // handle success
+    //       // console.log(response.data.data);
+    //       tray.value = [...response.data.data]
+    //       setstuffs([...response.data.data])
+          
+    //       // console.log(tray.value.length)
+    //     })
+    //     .catch(function (error) {
+    //       // handle error
+    //       console.log(error);
+    //     })
+    //     .finally(function () {
+    //       // always executed
+    //     });
+    // }
     // console.log(tray.value)
     
   
-    console.log(tray.value.length)
+    // console.log(tray.value.length)
     let tem = [];
   
     // tem = stuffs.slice(1,10);
-    tem = tray.value.slice(1,10);
-
+    
+    for (let i =0; i < tray.value.slice(1,10).length;i++){
+      tem.push(hmap.get(tray.value[i]));
+    }
     console.log(tem)
     return (
   
@@ -51,12 +78,13 @@ export  function Home() {
           <input
             className='search'
             type="text"
-            onKeyDownCapture={e => handleQuery(e.target.value)}
+            onKeyDownCapture={e => search(e.target.value)}
           />
   
           <input type='submit' className='btn-search'/>
         </div>
         {tem.map(x=> <p>{x.q}</p>)}
+        
       </div>
   
     )
